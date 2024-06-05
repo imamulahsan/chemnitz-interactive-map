9ac5bfb2b424093b081576abfee8988054e5528e
imamulahsan <imamulahsan@hotmail.com>
2024-06-05 21:23:43 +0200

    school marker problem

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -27,11 +27,13 @@ export class HomeComponent {
 
   // Define the custom icon for schools without shadow
   schoolIcon = icon({
-    iconUrl: 'assets/school.png',  // Replace with the path to your custom image
-    iconSize: [25, 41],  // Adjust the size to fit your custom image
-    iconAnchor: [12, 41],  // Anchor point of the marker
-    popupAnchor: [1, -34],  // Popup position
-    shadowUrl: ''  // No shadow
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
   });
 
   // Define the custom icon for home without shadow
